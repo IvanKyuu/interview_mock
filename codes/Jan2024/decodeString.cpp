f3a8be7fe4555f7645e947308b9a39b512095378
ivanshan_8170 <ivan.shan@huawei.com>
2024-01-24 00:46:25 -0500

    working on the second summary

---
+++ codes/Jan2024/decodeString.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+
+class Solution {
+public:
+    string decodeString(string s) {
+        vector<string> stack = {};
+        // TODO(ivanShan)
+        for (int i = 0; i < s.size(); ++i) {
+            if s[i] == 0;
+        };
+
+        return "";
+    }
+};
+
+
+int main() {
+    Solution s = Solution();
+    cout << s.decodeString("2[abc]3[cd]ef") << endl;
+}
+
+
---
+++ note/README.md
@@ -0,0 +1,8 @@
+# Front
+Page for noting my mock interview summary.
+ 
+
+## Useful Links:
+
+[C++ Developer Roadmap](https://roadmap.sh/cpp)
+
