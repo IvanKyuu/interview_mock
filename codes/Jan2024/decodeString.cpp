999b87ca3cb1adc20438c8aa8a2ea667150e277b
ivanshan_8170 <ivan.shan@huawei.com>
2024-01-24 22:40:14 -0500

    complete the second mock editting

--- codes/Jan2024/decodeString.cpp
+++ codes/Jan2024/decodeString.cpp
@@ -1,30 +1,61 @@
+#include <cctype>
 #include <iostream>
 #include <string>
+#include <utility>
 #include <vector>
+#include <stack>
 
-using std::vector;
+using std::atoi;
 using std::cout;
 using std::endl;
+using std::make_pair;
+using std::pair;
 using std::string;
+using std::vector;
 
-
-class Solution {
+class Solution
+{
 public:
-    string decodeString(string s) {
-        vector<string> stack = {};
-        // TODO(ivanShan)
-        for (int i = 0; i < s.size(); ++i) {
-            if s[i] == 0;
-        };
+    std::string decodeString(const std::string &s)
+    {
+        std::stack<std::pair<int, std::string>> stack;
+        std::string current = "";
+        int count = 0;
 
-        return "";
+        for (char item : s)
+        {
+            if (isdigit(item))
+            {
+                count = count * 10 + (item - '0');
+            }
+            else if (item == '[')
+            {
+                stack.push({count, current});
+                current = "";
+                count = 0;
+            }
+            else if (item == ']')
+            {
+                auto last = stack.top();
+                stack.pop();
+                string temp = "";
+                for (int j = 0; j < last.first; ++j) {
+                    temp.append(current);
+                }
+                current = last.second + temp;
+            }
+            else
+            {
+                current += item;
+            }
+        }
+
+        return current;
     }
 };
 
-
-int main() {
+int main()
+{
     Solution s = Solution();
     cout << s.decodeString("2[abc]3[cd]ef") << endl;
 }
-
-
